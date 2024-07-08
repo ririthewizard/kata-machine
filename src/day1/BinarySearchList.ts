1bd5911c14910c21d54dab2598d59fd226808eb0
Riley <ririwilliams.rw@gmail.com>
2024-07-07 22:56:35 -0500

    Finished linear search which is O(n) time complexity because the time it takes grows with our input. Currently working on Binary Search. Unsure why it is returning false for one test case

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        let mid = (lo + (hi - lo) / 2);
+        if (haystack[mid] === needle){
+            return true;
+        } else if (needle > haystack[mid]){
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    } while (lo < hi)
+
+    return false;
+}
